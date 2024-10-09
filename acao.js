d4c10679db2ae775d999eac5ac784f54a6fc2ae7
Kethelyn-Sarah <143228852+Kethelyn-Sarah@users.noreply.github.com>
2024-10-09 06:36:41 -0300

    Add files via upload

--- acao.js
+++ acao.js
@@ -23,7 +23,13 @@ document.getElementById("btn-ler-mais").addEventListener("click", function() {
 //ptrcisa funcionar
 
 let data = new Date();
-let diaNumrto = data.getDate();
+let diaNumrto = data.getDay();
+let dia = data.getDate();
 
-let  ativo = document.querySelector(".mini-calender li:nth-child('diaNumrto')");
+
+let  ativo = document.querySelector(".mini-calender li:nth-child("+diaNumrto+")");
 ativo.classList.add("atual");
+
+let h1 = document.createElement('h1');
+h1.innerHTML = dia;
+ativo.appendChild(h1);
