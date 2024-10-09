1c6ae757669d7bbbd9b2723428bd16c3ef03440c
Kethelyn-Sarah <143228852+Kethelyn-Sarah@users.noreply.github.com>
2024-10-09 15:53:59 -0300

    Add files via upload

--- acao.js
+++ acao.js
@@ -22,10 +22,12 @@ document.getElementById("btn-ler-mais").addEventListener("click", function() {
 //mini clendartio
 //ptrcisa funcionar
 
+let meses=['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+
 let data = new Date();
 let diaNumrto = data.getDay();
 let dia = data.getDate();
-
+let mes = meses[data.getMonth()];
 
 let  ativo = document.querySelector(".mini-calender li:nth-child("+diaNumrto+")");
 ativo.classList.add("atual");
@@ -33,3 +35,7 @@ ativo.classList.add("atual");
 let h1 = document.createElement('h1');
 h1.innerHTML = dia;
 ativo.appendChild(h1);
+
+let h5 = document.createElement('h5');
+h5.innerHTML = mes;
+ativo.appendChild(h5);
